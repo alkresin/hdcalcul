96eeb44d72ffa3e8f4a1d542a48f59415911bee3
alkresin <alkresin@yahoo.com>
2015-03-31 20:48:23 +0300

    java files updates due to hDroidGUI changes

--- src/su/harbour/hdcalcul/DopActivity.java
+++ src/su/harbour/hdcalcul/DopActivity.java
@@ -1,59 +1,14 @@
 package su.harbour.hdcalcul;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.content.Context;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
 import su.harbour.hDroidGUI.*;
 
-public class DopActivity extends Activity {
-
-    private View mainView;
-    private String sId;
+public class DopActivity extends HDActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-       Intent intent = getIntent();   
-       String sAct = intent.getStringExtra("sact");
-       sId = intent.getStringExtra("sid");
-
-       mainView = MainApp.harb.createAct( this, sAct );
-       //MainApp.harb.hlog("Dop - onCreate "+sId+sAct);
-       setContentView( mainView );
-       MainApp.harb.hrbCall( "HD_INITWINDOW",sId );
-    }
-
-   @Override
-   protected void onResume() {
-       super.onResume();
-
-      //MainApp.harb.hlog("Dop - onResume");
-      MainApp.harb.setContext( this, mainView );
-   }
-
-   @Override
-   protected void onDestroy() {
-       super.onDestroy();
-
-       //MainApp.harb.hlog("Dop - onDestroy "+sId);
-       MainApp.harb.closeAct( sId );
-   }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-       MainApp.harb.SetMenu( menu );
-       return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-       MainApp.harb.onMenuSel( item.getItemId() );
-       return true;
     }
 
 }
--- src/su/harbour/hdcalcul/MainActivity.java
+++ src/su/harbour/hdcalcul/MainActivity.java
@@ -1,53 +1,16 @@
 package su.harbour.hdcalcul;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import su.harbour.hDroidGUI.*;
 
-public class MainActivity extends Activity {
-
-   private static View mainView;
+public class MainActivity extends HDActivity {
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
 
-      //MainApp.harb.hlog("Main - onCreate");
+      bMain = true;
       MainApp.harb.setDopClass( DopActivity.class );
-      mainView = MainApp.harb.createAct( this, null );
-      setContentView( mainView );
-      MainApp.harb.hrbCall( "HD_INITWINDOW","0" );
+      super.onCreate(savedInstanceState);
    }
-
-   @Override
-   protected void onResume() {
-       super.onResume();
-
-      //MainApp.harb.hlog("Main - onResume");
-      MainApp.harb.setContext( this, mainView );
-   }
-
-   @Override
-   protected void onDestroy() {
-       super.onDestroy();
-
-       //MainApp.harb.hlog("Main - onDestroy");
-       MainApp.harb.closeAct( "0" );
-   }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-       MainApp.harb.SetMenu( menu );
-       return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-       MainApp.harb.onMenuSel( item.getItemId() );
-       return true;
-    }
 
 }
